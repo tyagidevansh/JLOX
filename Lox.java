6b4844e9c28fc25fb184c3c6a77f30a125585508
tyagidevansh <devanshtyagi0607@gmail.com>
2024-10-15 23:25:30 +0530

    scanner class

---
+++ Lox.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+  static boolean hadError = false;
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64); 
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+
+  //start it from the command line and give it a path of a file to execute and it'll do it
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+    if (hadError) System.exit(65);
+  }
+
+  //run interactively, enter and execute code on the command line one line at a time, ctrl D for EOL  
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) { 
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null) break;
+      run(line);
+      hadError = false;
+    }
+  }
+
+
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+
+    // For now, just print the tokens.
+    for (Token token : tokens) {
+      System.out.println(token);
+    }
+  }
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where,
+                             String message) {
+    System.err.println(
+        "[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }
+}
---
+++ Scanner.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+import static TokenType.*;
+
+class Scanner {
+  private final String source;
+  private final List<Token> tokens = new ArrayList<>();
+  private int start = 0;
+  private int current = 0;
+  private int line = 1;
+
+  Scanner(String source) {
+    this.source = source;
+  }
+
+  List<Token> scanTokens() {
+    while (!isAtEnd()) {
+      // We are at the beginning of the next lexeme.
+      start = current;
+      scanToken();
+    }
+
+    tokens.add(new Token(EOF, "", null, line));
+    return tokens;
+  }
+
+  private boolean isAtEnd() {
+    return current >= source.length();
+  }
+
+  private void scanToken() {
+    char c = advance();
+    switch (c) {
+        case '(': addToken(TokenType.LEFT_PAREN); break;
+        case ')': addToken(TokenType.RIGHT_PAREN); break;
+        case '{': addToken(TokenType.LEFT_BRACE); break;
+        case '}': addToken(TokenType.RIGHT_BRACE); break;
+        case ',': addToken(TokenType.COMMA); break;
+        case '.': addToken(TokenType.DOT); break;
+        case '-': addToken(TokenType.MINUS); break;
+        case '+': addToken(TokenType.PLUS); break;
+        case ';': addToken(TokenType.SEMICOLON); break;
+        case '*': addToken(TokenType.STAR); break;
+    }
+}
+
+
+  private char advance() {
+    return source.charAt(current++);
+  }
+
+  private void addToken(TokenType type) {
+    addToken(type, null);
+  }
+
+  private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
+  }
+  
+}
