bd2fa2034c4baae62ed60ae2070f192eea3758e0
tyagidevansh <devanshtyagi0607@gmail.com>
2024-10-16 20:09:44 +0530

    scanner

--- Scanner.java
+++ Scanner.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import static TokenType.*;
+import java.util.Map;
 
 class Scanner {
   private final String source;
@@ -8,6 +9,28 @@ class Scanner {
   private int start = 0;
   private int current = 0;
   private int line = 1;
+
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",    TokenType.AND);
+    keywords.put("class",  TokenType.CLASS);
+    keywords.put("else",   TokenType.ELSE);
+    keywords.put("false",  TokenType.FALSE);
+    keywords.put("for",    TokenType.FOR);
+    keywords.put("fun",    TokenType.FUN);
+    keywords.put("if",     TokenType.IF);
+    keywords.put("nil",    TokenType.NIL);
+    keywords.put("or",     TokenType.OR);
+    keywords.put("print",  TokenType.PRINT);
+    keywords.put("return", TokenType.RETURN);
+    keywords.put("super",  TokenType.SUPER);
+    keywords.put("this",   TokenType.THIS);
+    keywords.put("true",   TokenType.TRUE);
+    keywords.put("var",    TokenType.VAR);
+    keywords.put("while",  TokenType.WHILE);
+  }
 
   Scanner(String source) {
     this.source = source;
@@ -20,7 +43,7 @@ class Scanner {
       scanToken();
     }
 
-    tokens.add(new Token(EOF, "", null, line));
+    tokens.add(new Token(TokenType.EOF, "", null, line));
     return tokens;
   }
 
@@ -41,9 +64,128 @@ class Scanner {
         case '+': addToken(TokenType.PLUS); break;
         case ';': addToken(TokenType.SEMICOLON); break;
         case '*': addToken(TokenType.STAR); break;
-    }
-}
 
+        //check length two lexemes 
+        case '!':
+          addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG);
+          break;
+        case '=':
+          addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL);
+          break;
+        case '<':
+          addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS);
+          break;
+        case '>':
+          addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER);
+          break;
+          
+        //we need to check if '/' means division or the first character of a comment  
+        case '/':
+          if (match('/')) {
+            // A comment goes until the end of the line.
+            while (peek() != '\n' && !isAtEnd()) advance();
+          } else {
+            addToken(TokenType.SLASH);
+          }
+          break;
+        
+        case ' ':
+        case '\r':
+        case '\t':
+          // Ignore whitespace.
+          break;
+
+        case '\n':
+          line++;
+          break;
+        
+        //string literal
+        case '"': string(); break;
+
+        default:
+          if (isDigit(c)) {
+            number();
+          } else if (isAlpha(c)) {
+            identifier();
+          } else {
+            Lox.error(line, "Unexpected character.");
+          }
+      }
+  }
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+
+    current++;
+    return true;
+  }
+
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') line++;
+      advance();
+    }
+
+    if (isAtEnd()) {
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+
+    // The closing ".
+    advance();
+
+    // Trim the surrounding quotes.
+    String value = source.substring(start + 1, current - 1);
+    addToken(TokenType.STRING, value);
+  }
+
+  private void number() {
+    while (isDigit(peek())) advance();
+
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "."
+      advance();
+
+      while (isDigit(peek())) advance();
+    }
+
+    addToken(TokenType.NUMBER,
+        Double.parseDouble(source.substring(start, current)));
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = TokenType.IDENTIFIER;
+    addToken(type);
+  }
+
+  private char peek() {
+    if (isAtEnd()) return '\0';
+    return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  } 
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+            c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
 
   private char advance() {
     return source.charAt(current++);
