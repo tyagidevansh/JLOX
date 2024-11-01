e275f70f41473619402dc93e74314194ec445378
tyagidevansh <devanshtyagi0607@gmail.com>
2024-11-01 23:46:00 +0530

    resolving and binding

--- Environment.java
+++ Environment.java
@@ -29,6 +29,23 @@ class Environment {
     values.put(name, value); //allows re-definition of variables
   }
 
+  Environment ancestor(int distance) {
+    Environment environment = this;
+    for (int i = 0; i < distance; i++) {
+      environment = environment.enclosing; 
+    }
+
+    return environment;
+  }
+
+  Object getAt(int distance, String name) {
+    return ancestor(distance).values.get(name);
+  }
+
+  void assignAt(int distance, Token name, Object value) {
+    ancestor(distance).values.put(name.lexeme, value);
+  }
+
   void assign(Token name, Object value) {
     if (values.containsKey(name.lexeme)) {
       values.put(name.lexeme, value);
--- Lox.java
+++ Lox.java
@@ -59,6 +59,11 @@ public class Lox {
     // Stop if there was a syntax error.
     if (hadError) return;
 
+    Resolver resolver = new Resolver(interpreter);
+    resolver.resolve(statements);
+    
+    if (hadError) return;
+
     //System.out.println(new AstPrinter().print(expression));
     interpreter.interpret(statements); //3. evaluate statements
   }
